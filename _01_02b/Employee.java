374d40383aca3be9361b41f31e216721d2b89b7c
seebass15 <seebassquinn.55@gmail.com>
2025-01-07 10:45:47 -0500

    Update Employee.java

--- _01_02b/Employee.java
+++ _01_02b/Employee.java
@@ -5,9 +5,9 @@ public class Employee {
   public static void main(String[] args) {
 
     // Create a variable called age of type int and assign it the value 29.
-
+int age = 29;
     // Print the age variable to the console.
-
+System.out.println(age);
     // Create a variable called isAManager of type boolean and assign it the value
     // true.
 
