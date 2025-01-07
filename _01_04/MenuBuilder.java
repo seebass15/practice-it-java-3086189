51688bd2fb0f2238ba022b76b0dd6b289d121141
seebass15 <seebassquinn.55@gmail.com>
2025-01-07 11:03:46 -0500

    Update MenuBuilder.java

--- _01_04/MenuBuilder.java
+++ _01_04/MenuBuilder.java
@@ -7,26 +7,36 @@ public class MenuBuilder {
 
     // Create a variable called menuTitle of type String and assign it the value "My
     // Dream Menu:".
+    String menuTitle = 'My Dream Menu:';
 
     // Print the menuTitle variable to the console.
+    System.out.println(menuTitle);
 
     // Create a variable called menu of type ArrayList.
+    ArrayList menu = new ArrayList<>;
 
     // Create a variable called starter of type MenuItem and pass in the name of
     // your favourite starter.
+    MenuItem starter = 'calamari';
 
     // Add the starter variable to the ArrayList called menu.
+    menu.add(starter);
 
     // Create a variable called mainCourse of type MenuItem and pass in the name of
     // your favourite main course.
+    MenuItem mainCourse = 'lasagna';
 
     // Add the mainCourse variable to the ArrayList called menu.
+    menu.add(mainCourse);
 
     // Create a variable called dessert of type MenuItem and pass in the name of
     // your favourite dessert.
+    MenuItem dessert = 'ice cream';
 
     // Add the dessert variable to the ArrayList called menu.
-
+    menu.add(dessert);
+    
     // Print the menu variable to the console.
+    System.out.println(menu);
   }
 }
